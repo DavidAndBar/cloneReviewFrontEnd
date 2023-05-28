f82abe3584ed11fea21c84f095bf38d93170d4db
DavidAndBar <davidbar22@hotmail.com>
2023-05-28 12:48:16 -0400

    This is until the proposed in the front end, class 2 at 1:40:57

---
+++ firstPractice/function_exercises.js
@@ -0,0 +1,18 @@
+// This is the script to practice execution of functions in JS.
+
+// Arrow Functions
+
+// Without Parameters:
+const generalGreeting = () => {console.log("Hi World!");};
+generalGreeting();
+
+// Simpler:
+const generalGreetingV2 = () => "Hi Simpler World!";
+console.log(generalGreetingV2());
+
+// With parameters:
+const specificGreeting = (name) => {console.log(`Hi ${name}`);};
+specificGreeting("Peter");
+
+// Now an IIFE (Immediately Invoked Function Expression) arrow function:
+(() => console.log("Hi from an IIFE"))();
