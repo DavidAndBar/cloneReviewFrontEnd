4af386292d86fda1ec581ada8e93595e422c6416
DavidAndBar <davidbar22@hotmail.com>
2023-05-28 23:01:33 -0400

    even if you forget to push your code before turn off your pc, you can still do it next session.

--- firstPractice/destructuring.js
+++ firstPractice/destructuring.js
@@ -1,15 +1,44 @@
 const numbers = [];
 const obj = {
-    a: 1,
-    b: 2,
-    c: 3,
-    d: 5
+    firstNumber: 1,
+    secondNumber: 2,
+    thirdNumber: 10,
+    fourthNumber: 5
 };
 
-// This needs to be reviewed, idk yet what is this for.
-({ a: numbers[0], b: numbers[1] } = obj);
+// This needs to be reviewed, idk yet what can this be used for.
+({ firstNumber: numbers[0], secondNumber: numbers[1] } = obj);
 console.log(numbers);
 console.log(obj);
 // This statement destructure an object.
-const {b , d} = obj;
-console.log(b, d);
+const {secondNumber , fourthNumber} = obj;
+console.log(secondNumber, fourthNumber);
+
+//  Example of one function using destructuring:
+// It's curious that setting a variable outside the function, still can be used. 
+
+const outsideVariable = 1000;
+
+const compareNumbers = (obj) => {
+    const {thirdNumber, fourthNumber} = obj;
+    if (thirdNumber > fourthNumber){
+        console.log(`${thirdNumber} is greater than ${fourthNumber}, and the ouside variable is ${outsideVariable}`);
+    } else {
+        const {firstNumber} = obj;
+        console.log(`${fourthNumber} is greater than ${thirdNumber} and the first number is ${firstNumber}`);
+    }
+}
+compareNumbers(obj);
+
+// We can also destructure functions:
+const consultName = () => {
+    const name = "David";
+    const getName = () => {
+        return name;
+    }
+    return [name, getName()];
+}
+
+const [variableName, functionGetName] = consultName();
+console.log(`${variableName}`);
+console.log(`${functionGetName}`);
