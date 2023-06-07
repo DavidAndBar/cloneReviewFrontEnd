0d54e5bc05b4a1b2482b4b1c0feaf08da7eafb35
DavidAndBar <davidbar22@hotmail.com>
2023-06-07 16:00:53 -0400

    this is the migration of Twitter app to React

--- practiceHTML/twitterReactApp/twitterreact/src/index.js
+++ practiceHTML/twitterReactApp/twitterreact/src/index.js
@@ -1,15 +1,14 @@
-// This file was created as .js at first, but will be modified to be .jsx to support
-// coding merging both javascript and HTML syntax.
+// This file was created as .js at first, but it's really a .jsx to support
+// coding merging both javascript and HTML syntax, but it works also as a .js extension.
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import Main from './components/main/Main';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App /> {/* This is a comment in jsx, This is not a HTML Tag, it's a component written in jsx*/}
+    <Main /> {/* This is a comment in jsx, This is not a HTML Tag, it's a component written in jsx*/}
   </React.StrictMode>
 );
 
