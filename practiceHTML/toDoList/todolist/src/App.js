134b59f60a7d8ba7eace183c3ed3c9ac6d0b5388
DavidAndBar <davidbar22@hotmail.com>
2023-06-08 23:02:08 -0400

    To do app structure it's created, it's pending add key features

--- practiceHTML/toDoList/todolist/src/App.js
+++ practiceHTML/toDoList/todolist/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react"
+import Form from "./components/form/Form.jsx"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+  
+  const initItem = [];
+
+  const [item, setItem] = useState(initItem);
+  
+  return <>
+          <section>
+            <h2> To do list: </h2>
+            <Form item={item} setItem={setItem}/>
+          </section>
+        </>
+  ;
 }
 
 export default App;
---
+++ practiceHTML/toDoList/todolist/src/components/form/Form.jsx
@@ -0,0 +1,50 @@
+import {useState} from "react"
+
+const Form = ({ item, setItem}) => {
+    
+
+    const AddItems = () => {
+        const [input, setInput] = useState("");
+        const handleChange = (event) => { /* event is called to the change in the element that is being called (input) */
+            setInput(event.target.value);
+        }
+
+        const handleClick = () => {
+            const listAuxiliar = [...item]; /* It's copied this way because as it's an array. 
+                                            To make a copy of an object or array it must be by reference and not as we traditionaly do (as values) */
+            listAuxiliar.push({
+                id: Date.now(),
+                task: input,
+            completed: false});
+            setItem(listAuxiliar);
+            setInput("");
+            /*setItem(item)*/
+        }        
+
+        return <p>  <input type="text" onChange={handleChange} value={input} placeholder="Add a task"/>
+                    <button type="button" onClick={handleClick}>  Add task </button>
+                </p>
+    }
+
+    const ItemList = () => {
+        return <>
+            { item.length === 0 ?
+            <>
+            <p>No tasks</p> 
+            </>:
+            <ul>
+                {
+                    item.map( item => <li key={item.id}> {item.task} </li>)
+                }
+            </ul>}</>
+    
+    }
+
+    return <>
+        <AddItems />
+        <ItemList />
+        </>;
+
+}
+
+export default Form
