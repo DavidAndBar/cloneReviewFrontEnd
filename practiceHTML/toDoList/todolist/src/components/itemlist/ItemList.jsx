b22b3d768fea4a790b13f47b77bd3cde072deda2
DavidAndBar <davidbar22@hotmail.com>
2023-06-11 13:43:10 -0400

    Found a way to collect the value of my id in the function to be updated in my task array

--- practiceHTML/toDoList/todolist/src/components/itemlist/ItemList.jsx
+++ practiceHTML/toDoList/todolist/src/components/itemlist/ItemList.jsx
@@ -1,31 +1,46 @@
-// import { useState } from "react";
+import { useState } from "react";
 
 const ItemList = ({item, setItem}) => {
 
-    const isChecked = (id) => { // I don't understand this very well, why is the id in this line exactly the one I want?
+    const [checkedBox, setCheckedBox] = useState(false);
+
+    const isChecked = (checkbox) => { // I don't understand this very well, why is the id in this line exactly the one I want?
+        console.log(checkbox.target.checked);
+        console.log(checkbox.target.value);
+        if (checkbox.target.checked === true) {
+            setCheckedBox(true);
+        } else {
+            setCheckedBox(false);
+        }
+
+        /*if(checkbox.checked) {
+            console.log(checkbox.value+"True")
+        }
+        else {
+            console.log(checkbox.value+"False")
+        }
+        console.log("id: ",id);
         console.log("Looking for an id", item.find(o => o.id === id)); // This is just a test, I can delete in a while
         const checkedItem = item.find(o => o.id === id).id; // Save the entire entry
         const indexItem = item.indexOf(item.find(o => o.id === id)); // Look for the position of the entry i'm managing
-        
         console.log("item: ", item);
         console.log("item[indexItem]: ", item[indexItem]);
         console.log("indexItem: ", indexItem);
+        console.log("CheckedItem: ", checkedItem);
         console.log("item[indexItem].completed: ", item[indexItem].completed);
         console.log("After this comes the if");
-        if (item[indexItem].completed === false) { 
+        if (item[indexItem].completed === false) {
             item[indexItem].completed = true;
+            setItem(item);
         } else {
             item[indexItem].completed = false;
+            setItem(item);
         }
-        
         console.log(item);
         console.log(item[indexItem]);
         console.log(indexItem);
-        console.log(item[indexItem].completed);
-    };
-
-    const styleRow = {
-
+        console.log("CheckedItem: ", checkedItem);
+        console.log(item[indexItem].completed);*/
     };
 
     return <>
@@ -33,8 +48,8 @@ const ItemList = ({item, setItem}) => {
         <p>No tasks</p>:
         <ul>
             {
-                item.map( item => <><li key={item.id} style={styleRow}> {item.task} {item.completed} 
-                                    <input type="checkbox" onClick={isChecked(item.id)}/> 
+                item.map( item => <><li key={item.id}> {item.task} {item.id}
+                                    <input type="checkbox" onChange={isChecked} checked={item.completed} value={item.id}/>
                                     <button type="button">Delete Task</button>
                                     </li></>)
             }
