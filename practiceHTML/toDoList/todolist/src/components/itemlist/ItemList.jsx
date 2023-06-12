ec5887f385e4c14aad873c52dfe069549cbf1c1f
DavidAndBar <davidbar22@hotmail.com>
2023-06-12 15:03:54 -0400

    The styles were updated, next task is making sure the delete button works properly

--- practiceHTML/toDoList/todolist/src/components/itemlist/ItemList.jsx
+++ practiceHTML/toDoList/todolist/src/components/itemlist/ItemList.jsx
@@ -8,8 +8,14 @@ const ItemList = ({item, setItem}) => {
                                     the completed attribute is true, once we click the button that attribute is set as false, same logic if it's false
                                     it turns true.*/
         const indexItem = item.indexOf(item.find(o => o.id === parseInt(checkbox.target.value))); // value is a string, so it needs to be transformed in int.
-        item[indexItem].completed = !item[indexItem].completed
-        console.log(item);
+        const modifingEntry = item[indexItem];
+        modifingEntry.completed = !modifingEntry.completed;
+        const newArray = [...item];
+        newArray.splice(indexItem,1,modifingEntry);
+        setItem(newArray); 
+        /*item[indexItem].completed = !item[indexItem].completed;   This way to update the entry doesn't work because it doesn't render the changes
+                                                                    in the item array, use pass-by-reference*/
+        console.log(item); // this can be deleted in the future since it's only to check the checkbox it's working
     };
 
     return <>
@@ -27,7 +33,7 @@ const ItemList = ({item, setItem}) => {
                 item.map( ({ id, task, completed}) => 
                 <tbody key={id}>
                     <tr key={`row${id}`} className="row">
-                        <td key={`task${id}`} className={`task-${ completed ? "" : "completed"}`}>{task}</td>
+                        <td key={`task${id}`} className={`task-${ completed ? "completed" : ""}`}>{task}</td>
                         <td key={`input${id}`}><input type="checkbox" onClick={isChecked} value={id} defaultChecked={completed}/></td>
                         {/* This input is type checkbox to make it have 2 options, int his case, the task is completed or not. onClick attribute activate the 
                         function isChecked once it's clicked (more info in isChecked function). Value is setted as item.id as I need to give that value to 
