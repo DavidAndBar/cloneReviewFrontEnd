ccf6628d23cc7debc751ac6ff0d1f5ed98296c8c
DavidAndBar <davidbar22@hotmail.com>
2023-06-12 19:29:24 -0400

    Now, the next step it's divide the 2 item lists, the completed one and the uncompleted, now when I add a new task and there are some complete tasks, it goes in the last position of the list, I need make the new task to be first than the checked ones

--- practiceHTML/toDoList/todolist/src/components/itemlist/ItemList.jsx
+++ practiceHTML/toDoList/todolist/src/components/itemlist/ItemList.jsx
@@ -12,6 +12,14 @@ const ItemList = ({item, setItem}) => {
         modifingEntry.completed = !modifingEntry.completed;
         const newArray = [...item];
         newArray.splice(indexItem,1,modifingEntry);
+        if (newArray[indexItem].completed === true) { // This if it's set to put in the last of the list completed items, if they are unchecked, then they go to the top of the list.
+            newArray.push(modifingEntry);
+            newArray.splice(indexItem,1);
+        } else {
+            const indexUncompleted = item.indexOf(item.find(o => o.id === parseInt(checkbox.target.value)));
+            newArray.splice(indexUncompleted,1);
+            newArray.splice(0,0,modifingEntry);
+        }
         setItem(newArray); 
         /*item[indexItem].completed = !item[indexItem].completed;   This way to update the entry doesn't work because it doesn't render the changes
                                                                     in the item array, use pass-by-reference*/
@@ -42,14 +50,13 @@ const ItemList = ({item, setItem}) => {
                     <tr key={`row${id}`} className="row">
                         <td key={`task${id}`} className={`task-${ completed ? "completed" : ""}`}>{task}</td>
                         <td key={`input${id}`}><input type="checkbox" onClick={isChecked} value={id} defaultChecked={completed}/></td>
-                        {/* This input is type checkbox to make it have 2 options, int his case, the task is completed or not. onClick attribute activate the 
+                        {/* This input is type checkbox to make it have 2 options, in this case, the task is completed or not. onClick attribute activate the 
                         function isChecked once it's clicked (more info in isChecked function). Value is setted as item.id as I need to give that value to 
                         isChecked function since they can read that variable from event.target.value*/}
-                        <td key={`button${id}`}><button type="button" onClick={deleteButton} value={id}>Delete Task</button></td>
+                        <td key={`button${id}`}><button type="button" onClick={deleteButton} value={id}><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0H284.2c12.1 0 23.2 6.8 28.6 17.7L320 32h96c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 96 0 81.7 0 64S14.3 32 32 32h96l7.2-14.3zM32 128H416V448c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V128zm96 64c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16z"/></svg></button></td>
                     </tr>
                 </tbody>)
             }
-            
         </table>}
     </>
 }
