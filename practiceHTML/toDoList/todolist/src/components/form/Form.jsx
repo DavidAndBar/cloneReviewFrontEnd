af4e303ca1b6d1008a3a19730cd1cba2654bab5d
DavidAndBar <davidbar22@hotmail.com>
2023-06-18 22:58:12 -0400

    Rearrange the tasks to make it easier to save the values of the incompleted tasks in localStorage in browser. I need to give a better style for the lists

--- practiceHTML/toDoList/todolist/src/components/form/Form.jsx
+++ practiceHTML/toDoList/todolist/src/components/form/Form.jsx
@@ -3,15 +3,28 @@ import { useState } from "react";
 import "./style.css"
 
 const Form = () => {
-    const [tasks, setTasks] = useState([]);
+    const [numberOfLists, setNumberOfLists] = useState([]);
+    const [item1, setItem1] = useState(!!JSON.parse(localStorage.getItem("List1")) ? 
+                                        JSON.parse(localStorage.getItem("List1")) : 
+                                        []);
+    const [item2, setItem2] = useState(!!JSON.parse(localStorage.getItem("List2")) ? 
+                                        JSON.parse(localStorage.getItem("List2")) : 
+                                        []);
+    const [item3, setItem3] = useState(!!JSON.parse(localStorage.getItem("List3")) ? 
+                                        JSON.parse(localStorage.getItem("List3")) : 
+                                        []);
+    const [item4, setItem4] = useState(!!JSON.parse(localStorage.getItem("List4")) ? 
+                                        JSON.parse(localStorage.getItem("List4")) : 
+                                        []);
 
+    console.log(localStorage.getItem("List1"));
+    console.log(JSON.parse(localStorage.getItem("List1")));
+    console.log(item1);
     const newTask = () => {
-        if (tasks.length < 4) {
-            const auxNewTask = [...tasks];
-            auxNewTask.push(<form onSubmit={onSubmit} key={Date.now()}>
-                                <Task onSubmit={onSubmit}/>
-                            </form>);
-            setTasks(auxNewTask);
+        if (numberOfLists.length < 4) {
+            const newNumberOfList = [...numberOfLists];
+            newNumberOfList.push("List")
+            setNumberOfLists(newNumberOfList)
         } else {
             alert("Too many list of tasks")
         }
@@ -19,11 +32,19 @@ const Form = () => {
     }
 
     const deleteTask = () => {
-        const auxNewTask = [...tasks];
-        auxNewTask.splice(0,1);
-        setTasks(auxNewTask);
+        console.log("Running if");
+        const newNumberOfList = [...numberOfLists];
+        newNumberOfList.pop();
+        setNumberOfLists(newNumberOfList)
     }
 
+    const saveTask = () => {
+        localStorage.setItem("List1", JSON.stringify(item1))
+        localStorage.setItem("List2", JSON.stringify(item2))
+        localStorage.setItem("List3", JSON.stringify(item3))
+        localStorage.setItem("List4", JSON.stringify(item4))
+    }
+    
     const onSubmit = (e) => {
         e.preventDefault();
     }
@@ -32,9 +53,26 @@ const Form = () => {
         <div>
             <button type="button" className="newTaskButton" onClick={newTask}> New Day of tasks </button>
             <button type="button" className="deleteTaskButton" onClick={deleteTask}> Delete first list of tasks</button>
+            <button type="button" className="saveTasksButton" onClick={saveTask}> Save uncompleted tasks in local Storage</button>
         </div>
         <div style={{display: "flex"}}>
-            {tasks}
+            <form onSubmit={onSubmit} >
+            { numberOfLists.length > 0 && 
+            <><Task item={item1} setItem={setItem1}/></>}
+            </form>
+            <form onSubmit={onSubmit} >
+            { numberOfLists.length > 1 && 
+            <><Task item={item2} setItem={setItem2}/></>}
+            </form>
+            <form onSubmit={onSubmit} >
+            { numberOfLists.length > 2 && 
+            <><Task item={item3} setItem={setItem3}/></>}
+            </form>
+            <form onSubmit={onSubmit} >
+            { numberOfLists.length > 3 && 
+            <><Task item={item4} setItem={setItem4}/></>}
+            {/*tasks*/}
+            </form>
         </div>
     </>;
 
--- practiceHTML/toDoList/todolist/src/components/task/Task.jsx
+++ practiceHTML/toDoList/todolist/src/components/task/Task.jsx
@@ -1,21 +1,21 @@
-import { useState } from "react";
+//import { useState } from "react";
 import AddItems from "../addItems/AddItems";
 import ItemList from "../itemlist/ItemList";
 
-const Task = () => {
-    const [item, setItem] = useState([])
+const Task = ({item, setItem}) => {
+    //const [item, setItem] = useState([])
     const setDate = () => {
         const dateNow = new Date();
             
         return <>{`${dateNow.getDate()} - ${dateNow.getMonth() + 1 } - ${dateNow.getFullYear()}`}</>;
     }
 
-
     return <>
-        <h3 style={{textAlign: "center"}}> Created: {setDate()}  </h3>
-        <AddItems item={item} setItem={setItem}/>
-        <ItemList item={item} setItem={setItem}/>
-        </>;
+            <h3 style={{textAlign: "center"}}> Created: {setDate()}  </h3>
+            <AddItems item={item} setItem={setItem}/>
+            <ItemList item={item} setItem={setItem}/>
+        </>
+        ;
 }
 
 export default Task
